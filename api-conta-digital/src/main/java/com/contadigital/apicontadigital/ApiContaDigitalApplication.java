faa2b178e437664fce93a850e89fbc4ecd952a36
Iuri de Lima Marques <iurilmarques@hotmail.com>
2023-11-15 16:13:49 -0300

    added spring boot api for conta digital project

---
+++ api-conta-digital/src/main/java/com/contadigital/apicontadigital/ApiContaDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.contadigital.apicontadigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiContaDigitalApplication.class, args);
+	}
+
+}
---
+++ api-conta-digital/src/test/java/com/contadigital/apicontadigital/ApiContaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contadigital.apicontadigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiContaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
